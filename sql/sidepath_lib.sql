4ec2639e5103d80943427c6f58f3497a622544ce
Lu Fennell <lu-fennell@mailbox.org>
2025-07-14 23:56:04 +0200

    sql: fix (complete) csv output

--- sql/sidepath_lib.sql
+++ sql/sidepath_lib.sql
@@ -291,7 +291,7 @@ $$ LANGUAGE SQL;
 CREATE OR REPLACE FUNCTION sidepath_csv(buffer_distance float, buffer_size float)
   RETURNS TABLE (osm_id text, is_sidepath_estimation text) AS $$
     SELECT id::text as osm_id, sidepath_dict_is_sidepath(entry)::text FROM (
-      SELECT id, sidepath_dict_agg(nr, layer, road_id, tags) AS entry FROM sidepath_dict_checkpoints_and_roads_join(buffer_distance, buffer_size)
+      SELECT id, sidepath_dict_agg(nr, layer, road_id, tags) AS entry FROM sidepath_dict_checkpoints_and_roads_left_outer_join(buffer_distance, buffer_size)
       GROUP BY id
     )
   
